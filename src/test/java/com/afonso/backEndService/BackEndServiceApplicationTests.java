3fee2f86038bcc89ea68f31d8b4d91cb2dfb8434
Guilherme Afonso <ch.afonso.gui@gmail.com>
2020-12-27 12:08:01 -0300

    Projeto Criado

---
+++ src/test/java/com/afonso/backEndService/BackEndServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afonso.backEndService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
