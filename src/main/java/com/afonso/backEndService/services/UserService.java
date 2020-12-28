65a05675e76034711b07e9b75907c8ffa7493c43
Guilherme Afonso <ch.afonso.gui@gmail.com>
2020-12-28 14:58:21 -0300

    Atualização de User

--- src/main/java/com/afonso/backEndService/services/UserService.java
+++ src/main/java/com/afonso/backEndService/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.afonso.backEndService.entities.User;
 import com.afonso.backEndService.repositories.UserRepository;
+import com.afonso.backEndService.services.exceptions.ResourceNotFoundException;
 
 
 @Service
@@ -22,7 +23,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
