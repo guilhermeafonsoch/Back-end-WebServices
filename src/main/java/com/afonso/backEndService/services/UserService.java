7c272d2d41d969f9badba074003e46645656ae20
Guilherme Afonso <ch.afonso.gui@gmail.com>
2020-12-28 15:22:29 -0300

    Tratamento de exceção - delete

--- src/main/java/com/afonso/backEndService/services/UserService.java
+++ src/main/java/com/afonso/backEndService/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.afonso.backEndService.entities.User;
 import com.afonso.backEndService.repositories.UserRepository;
+import com.afonso.backEndService.services.exceptions.DatabaseException;
 import com.afonso.backEndService.services.exceptions.ResourceNotFoundException;
 
 
@@ -31,7 +34,16 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 	
 	public User update(Long id, User obj) {
